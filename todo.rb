54e87e38b5b7aaea47580d53cfa570bf196aa9e4
Andrew <overfitted@gmail.com>
2020-12-09 22:27:06 +0800

    fixed delete function, changed tasks to global variable

--- todo.rb
+++ todo.rb
@@ -1,15 +1,15 @@
-tasks = []
+$tasks = []
 is_done = false
 past_actions = []
 
 def delete
-  if tasks.empty?
+  if $tasks.empty?
     puts "no tasks to delete"
   else
     puts "which task would you like to delete?"
-    tasks.each{|task, index| puts "#{index + 1}: #{task}"}
-    target = gets
-    if target.between(1,tasks.length)
+    $tasks.each{|task, index| puts "#{index + 1}: #{task}"}
+    target = gets.chomp.to_i
+    if target.between(1,$tasks.length)
       tasks.delete_at(target - 1)
     else
       puts "please try again"
@@ -21,10 +21,10 @@ end
 
 until is_done
   puts "todo:"
-  if tasks.empty?
+  if $tasks.empty?
     puts "no tasks to do"
   else 
-    tasks.each{|task, index| puts "#{index + 1}: #{task}"}
+    $tasks.each{|task, index| puts "#{index + 1}: #{task}"}
   end
   puts "What would you like to do?\n1) delete task\n2) add task\n3) undo\n4) quit"
   option = gets.chomp
